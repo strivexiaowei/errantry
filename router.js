2ac959c0e22272362f3fae0587d17eedacfd8c50
huangxianwei <1064599729@qq.com>
2019-11-28 17:04:58 +0800

    xiugai

--- router.js
+++ router.js
@@ -13,12 +13,19 @@ const storge = multer.diskStorage({
       var fileformat = (file.originalname).split('.');
       cb(null, file.fieldname+'-'+Date.now()+'.'+fileformat[fileformat.length-1]);
   }
-})
+});
 const upload = multer({storage: storge});
+router.post('/BBS/dynamicList', function (req, res) {
+  const opts = req.body;
+  db.queryDynamicList(opts, function (result) {
+    res.json(result);
+  });
+});
 router.post('/BBS/createDynamic', function (req, res) {
   const opts = req.body;
   console.log(opts);
   opts.createTime = +Date.now();
+  opts.type = "Dy";
   db.addDynamic(opts, function (result) {
     if (result) {
       res.json({
@@ -31,13 +38,11 @@ router.post('/BBS/createDynamic', function (req, res) {
 
 router.post('/BBS/uploads', upload.array('file',20), function (req, res) {
   db.addFileImage(req.files[0], function (result) {
-    console.log(result);
     res.json(result);
   });
 });
 
 router.get('/uploads/images/*', function(req, res) {
-  console.log(req.url);
   const reqUrlArr = req.url.split('/');
   if (reqUrlArr[reqUrlArr.length - 1] === 'view') {
     let _id =  reqUrlArr.splice(-2)[0];
@@ -46,4 +51,62 @@ router.get('/uploads/images/*', function(req, res) {
     });
   }
 });
+
+router.post('/BBS/register', function(req, res) {
+  const opts = req.body;
+  db.registerBBS(opts,function (result) {
+    res.json(result);
+  });
+});
+router.post('/BBS/login', function(req, res) {
+ const opts = req.body;
+ db.loginBBS(opts, function (result) {
+   if (result) {
+     res.json({
+       code: 1,
+       desc: '登录成功',
+       result
+     });
+   } else {
+     res.json({
+       code: 2,
+       desc: '登录失败'
+     })
+   }
+ });
+});
+router.post('/BBS/appLogin', function (req, res) {
+  const opts = req.body;
+  db.loginBBS(opts, function (result) {
+    if (result) {
+      res.json({
+        code: 1,
+        desc: '登录成功'
+      });
+    } else {
+      db.addLogin(opts, function (json) {
+        res.json({
+          code: 1,
+          desc: '登录成功'
+        });
+      })
+    }
+  })
+});
+router.post('/BBS/getMsgList', function (req, res) {
+  const opts = {};
+  db.queryAllMsg(opts, function (result) {
+    res.json(result);
+  });
+});
+router.ws('/BBS/getMsg', function (ws, req) {
+  ws.on('message', function(msg) {
+    const opts =  JSON.parse(msg);
+    db.addOneRoomMsg(opts, function (result) {
+      if (result) {
+        ws.send(msg);
+      }
+    });
+  });
+})
 module.exports = router;
